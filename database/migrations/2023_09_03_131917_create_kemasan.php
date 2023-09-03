c16185918b4467a0fcc5dbac9002404db15ef301
Rifqi <arifqiza@gmail.com>
2023-09-03 20:46:23 +0700

    Table database migration baru

--- database/migrations/2023_09_03_131917_create_kemasan.php
+++ database/migrations/2023_09_03_131917_create_kemasan.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('kemasan', function (Blueprint $table) {
             $table->id();
+            $table->varchar('jenis_bungkus');
+            $table->integer('harga_bungkus');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2023_09_03_132949_create_batch_obat.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('batch_obat', function (Blueprint $table) {
+            $table->id();
+            $table->integer('harga_beli');
+            $table->integer('jumlah_masuk');
+            $table->integer('harga_jual');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('batch_obat');
+    }
+};
