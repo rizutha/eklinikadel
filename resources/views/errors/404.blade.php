4a4b969bee65b2e90898973eadd2f5d9641b5b4d
RizkiputraRamadhan <rizkikuliit@gmail.com>
2023-09-04 13:18:24 +0700

    memperbaiki bug 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,3 +1,9 @@
-error 404 
-
-jack rip buat tampilan 404 di folder errors/404 
+@extends('templates.default')
+@section('title', 'Tidak Ada')
+@php
+    $title = 'Halaman 404';
+    $preTitle = '';
+@endphp
+@section('content')
+<p>jack rif buat tampilan 404 di folder errors/404.blade.php</p>
+@endsection
